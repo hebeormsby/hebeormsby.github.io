181b845f28361e6a1d36f104d247daa122d449ea
hebeormsby <hebeormsby@gmail.com>
2020-10-31 21:34:49 -0400

    W07 Alarm

---
+++ js/alarm.js
@@ -0,0 +1,24 @@
+function alarm() {
+
+    // Input: collect date and time from computer
+    let now = new Date();
+    let month = now.getMonth();
+    let dayofMonth = now.getDate();
+    let dayofWeek = now.getDay();
+
+    // Processing: Define/check which days are considered a holiday
+    if (month == 0 && dayofMonth == 1 || month == 6 && dayofMonth == 4 || month == 11 && dayofMonth == 25) {
+        message = 'Sleep In!';
+    }
+
+    // Processing: Define/check which days are considered a weekday
+    else if (dayofWeek == 0 && dayofWeek == 6) {
+    message = 'Sleep In!';
+    }
+    else {
+    message = 'Get Up!';
+    }
+
+    //Output: displays message
+    document.getElementById('output').innerHTML = message;
+    }
