5c8bdf25df78b42bb34852abe9c144d0056d1a3c
hebeormsby <hebeormsby@gmail.com>
2020-10-31 21:48:09 -0400

    Update

--- js/alarm.js
+++ js/alarm.js
@@ -6,13 +6,8 @@ function alarm() {
     let dayofMonth = now.getDate();
     let dayofWeek = now.getDay();
 
-    // Processing: Define/check which days are considered a holiday
-    if (month == 0 && dayofMonth == 1 || month == 6 && dayofMonth == 4 || month == 11 && dayofMonth == 25) {
-        message = 'Sleep In!';
-    }
-
-    // Processing: Define/check which days are considered a weekend day
-    else if (dayofWeek === 0 || dayofWeek === 6) {
+    // Processing: Define/check which days are considered a holiday and weekend
+    if (month == 0 && dayofMonth == 1 || month == 6 && dayofMonth == 4 || month == 11 && dayofMonth == 25 || dayofWeek == 0 || dayofWeek == 6) {
         message = 'Sleep In!';
     }
     else {
