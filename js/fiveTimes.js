d67cacbf26f0ed1ecfd57017436b0bea39bade3d
hebeormsby <hebeormsby@gmail.com>
2020-11-14 15:40:06 -0500

    Upload

---
+++ js/fiveTimes.js
@@ -0,0 +1,11 @@
+function fiveTimes() {
+    //Input: define variables
+    let output = "";
+
+    //Processing: make a for loop repeat 12 times
+    for (let i = 1; i <= 12; i++) {
+        output += "5 x" + i + "=" + i*5 + '<br>';
+    }
+    //Ouput: display 5 times table between 1 - 12
+    document.getElementById('output').innerHTML = output;
+}
