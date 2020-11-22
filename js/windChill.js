b340d8d74c9f2b0ff09b01189548e36f96ead6a2
hebeormsby <hebeormsby@gmail.com>
2020-11-21 21:08:08 -0500

    Update

--- js/windChill.js
+++ js/windChill.js
@@ -20,5 +20,5 @@ function windChill(temperature, windspeed) {
     let windchill = 35.74 + 0.6215 * temperature - 35.75 * windspeed**0.16 + 0.4275 * temperature * windspeed**0.16;
     
     //Output: display calculated wind chill factor in fahrenheit
-    return document.getElementById('output').innerHTML = parseInt(windchill) + " degrees Fahrenheit";
+    return document.getElementById('output').innerHTML = parseInt(windchill) + " Degrees Fahrenheit";
 }
